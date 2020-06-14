82f57604b176d53b215d38f280a43eb89c9664a7
soeun <70000city@gmail.com>
2020-06-14 23:28:33 +0900

    미완성

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const moreBtn = document.querySelector(".info .metadata .moreBtn");
 const title = document.querySelector(".info .metadata .title");
 const description = document.querySelector(".description");
+const actionButton[] = document.querySelectorAll(".actions button");
 
 moreBtn.addEventListener("click", () => {
   moreBtn.classList.toggle("clicked");
   title.classList.toggle("clamp");
   description.classList.toggle("clamp");
+});
+
+actionButton.addEventListener("click", () => {
+  actionButton.classList.toggle("active");
 });
