63273bf55a4f8b086f9598a83865d2d174a2fd51
soeun <70000city@gmail.com>
2020-06-15 18:12:54 +0900

    Thumbs up&down/subscribe

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const moreBtn = document.querySelector(".info .metadata .moreBtn");
 const title = document.querySelector(".info .metadata .title");
 const description = document.querySelector(".description");
-const actionButton[] = document.querySelectorAll(".actions button");
+const actionButton = document.querySelectorAll("button.thumb");
+const subscribe = document.querySelector(".subscribe");
 
 moreBtn.addEventListener("click", () => {
   moreBtn.classList.toggle("clicked");
@@ -9,6 +10,14 @@ moreBtn.addEventListener("click", () => {
   description.classList.toggle("clamp");
 });
 
-actionButton.addEventListener("click", () => {
-  actionButton.classList.toggle("active");
+[].forEach.call(actionButton, function (a) {
+  a.addEventListener("click", actionButtonClick, false);
+});
+
+function actionButtonClick(e) {
+  this.classList.toggle("active");
+}
+
+subscribe.addEventListener("click", () => {
+  subscribe.classList.toggle("clicked");
 });
